d13c648b2e7cc1d3a802afef0d265f049fa25453
Kurtis Clauser <kclauser@gmail.com>
2015-10-02 12:32:59 -0400

    example added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002162243) do
+ActiveRecord::Schema.define(version: 20151002163121) do
 
   create_table "definitions", force: :cascade do |t|
     t.string   "word"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151002162243) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "part_of_speech"
+    t.string   "example"
   end
 
 end
